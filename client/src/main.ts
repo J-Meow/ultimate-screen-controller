e7669b9eeb30070a6492b550b55e44bfe509e973
Jmeow <j@jmeow.net>
2025-06-28 23:11:11 -0500

    Add size sending

---
+++ client/src/main.ts
@@ -0,0 +1,9 @@
+const ip = "127.0.0.1:9023"
+
+function start() {
+    const ws = new WebSocket("ws://" + ip)
+    ws.onopen = () => {
+        ws.send("size=" + innerWidth + "x" + innerHeight)
+    }
+}
+start()
--- server/main.ts
+++ server/main.ts
@@ -5,13 +5,18 @@ Deno.serve({ port: 9023 }, (req) => {
 
     const { socket, response } = Deno.upgradeWebSocket(req)
 
+    let width = 0
+    let height = 0
+
     socket.addEventListener("open", () => {
         console.log("Client connected")
     })
 
     socket.addEventListener("message", (event) => {
-        if (event.data === "ping") {
-            socket.send("pong")
+        if (event.data.startsWith("size=")) {
+            width = parseInt(event.data.split("size=")[1].split("x")[0])
+            height = parseInt(event.data.split("size=")[1].split("x")[1])
+            console.log("Size:", width, "x", height)
         }
     })
 
