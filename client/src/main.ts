9fc339f06db8d995891bc91f53fc356f10047fd2
Jmeow <j@jmeow.net>
2025-06-28 23:42:36 -0500

    Added canvas for drawing pixels on

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,12 @@ enum PacketType {
     Pixel = 1,
 }
 
+const mainCanvas = document.getElementById("main") as HTMLCanvasElement
+const ctx = mainCanvas.getContext("2d")!
+
+mainCanvas.width = innerWidth
+mainCanvas.height = innerHeight
+
 function uInt16ToInt(x: number, y: number) {
     return (y << 8) + x
 }
@@ -17,8 +23,8 @@ function start() {
         const data = await (ev.data as Blob).bytes()
         if (0 in data) {
             if (data[0] == PacketType.Pixel) {
-                console.log(uInt16ToInt(data[1], data[2]), uInt16ToInt(data[3], data[4]))
-                console.log(data[5], data[6], data[7])
+                ctx.fillStyle = `rgb(${data[5]}, ${data[6]}, ${data[7]})`
+                ctx.fillRect(uInt16ToInt(data[1], data[2]), uInt16ToInt(data[3], data[4]), 1, 1)
             }
         }
     }
